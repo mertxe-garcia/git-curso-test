29cae356eaffacf76605a3caceefff133a90184f
MERTXE GARCIA GITHUB <37287829+mertxe-garcia@users.noreply.github.com>
2018-03-13 08:24:52 +0100

    Add files via upload

---
+++ AssemblyInfo.vb
@@ -0,0 +1,32 @@
+Imports System.Runtime.InteropServices
+
+Imports System.Reflection
+
+' La información general de un ensamblado se controla mediante el siguiente
+' conjunto de atributos. Cambie estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Expertis.Sabico.Application.ERP.InsertarOtManitu")> 
+<Assembly: AssemblyDescription("Capa de presentación de Expertis")> 
+<Assembly: AssemblyCompany("Solmicro Organización y Software S.L.")> 
+<Assembly: AssemblyProduct("Expertis")> 
+<Assembly: AssemblyCopyright("Expertis®")> 
+<Assembly: AssemblyTrademark("Expertis")> 
+<Assembly: CLSCompliant(True)> 
+
+' La información de versión de un ensamblado consta de los siguientes cuatro valores:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Versión de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados (número de versión de compilación y de revisión) 
+' usando el símbolo '*' como se muestra a continuación:
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+
+<Assembly: AssemblyFileVersionAttribute("1.0.0.0")> 
+<Assembly: ComVisibleAttribute(False)> 
